6ad6fddd7eed21c0e9c6fb55fab2bd5b4496dc56
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-03-18 14:53:43 +0200

    added REST GET endpoint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -28,8 +29,19 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello world!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
