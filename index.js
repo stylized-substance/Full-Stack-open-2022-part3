8323e5a7ed98270bcc98448308ab798f782320b9
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-03-25 14:07:53 +0200

    created production build and added npm scripts

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms :body'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
