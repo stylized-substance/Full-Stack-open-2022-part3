ccce96f897b5c8dba0d2c4d8b9df9b6c6a6018e4
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-03-27 18:32:36 +0300

    mongo.js works

--- mongo.js
+++ mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('enter password as an argument');
+    console.log('Enter password as an argument');
     process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://fullstack:${password}@cluster0.e0mqdih.mongodb.net/phonebook?retryWrites=true&w=majority`
+`mongodb+srv://fullstack:${password}@cluster0.e0mqdih.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -20,15 +20,23 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const name = process.argv[3]
-const number = process.argv[4]
-
-const person = new Person({
-    name: name,
-    number: number,
-})
-
-person.save().then(result => {
-    console.log('person saved');
-    mongoose.connection.close()
-})
+if (process.argv.length < 4) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log('Phonebook:');
+            console.log(person.name, person.number)
+            mongoose.connection.close()
+        })
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+    person.save().then(result => {
+        console.log(`Added ${name} (number ${number}) to phonebook`);
+        mongoose.connection.close()
+    })
+}
