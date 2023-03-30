0ed2ca7c2e55527d39044c97b338fbdfec42d49f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-03-30 14:04:00 +0300

    added {new} option

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
