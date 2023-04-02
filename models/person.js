9b88d889768c3408885b732aef24dbc91ded213f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-02 16:46:44 +0300

    added number validation

--- models/person.js
+++ models/person.js
@@ -22,16 +22,21 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String,
         minLength: 8,
-        required: true
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /\d{2,3}-\d{7,8}/.test(v)
+            }
+        }
     }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+}
 })
 
 module.exports = mongoose.model('Person', personSchema)
