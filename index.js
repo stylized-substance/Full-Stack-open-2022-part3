fdeb81cc18aab20f53d6e215f7064173ec746366
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-03 17:41:44 +0300

    middleware fixes

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+// Define error handler middleware
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404.).send({ error: 'unknown endpoint' })
+}
+
+
 // Create body token for morgan
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
-app.use(morgan(':method :url :status :response-time ms :body'))
-app.use(express.json())
-app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms :body'))
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world!</h1>')
@@ -20,52 +37,52 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   Person.find({})
-    .then(persons => {
-      response.json(persons)
-    })
+  .then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
   let persons = Person.find({})
-    .then(searchResults => {
-      let dateObject = new Date()
-      response.send(`Phonebook has info for ${searchResults.length} people<br/><br/>${dateObject}`)
-    })
+  .then(searchResults => {
+    let dateObject = new Date()
+    response.send(`Phonebook has info for ${searchResults.length} people<br/><br/>${dateObject}`)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error)
-    )
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error)
+  )
 })
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
+  
   const person = new Person({
     name: body.name,
     number: body.number
   })
   person.save()
-    .then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
+  .then(savedPerson => {
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -74,29 +91,16 @@ app.put('/api/persons/:id', (request, response, next) => {
     name: body.name,
     number: body.number,
   }
-
+  
   Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
 })
 
-// Define error handler middleware
-
-const error = (error, request, response, next) => {
-  console.log('errorhandler run', error.name);
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    console.log('validationerror')
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(error)
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
